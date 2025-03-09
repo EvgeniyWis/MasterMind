897c1c6b2b5e2f0d82a404497121279811382b90
KrisWis <kworkEvgeniy@yandex.ru>
2025-03-09 13:42:00 +0300

    Add and initialize swiper for mainBlock, add new vars and flex classes, add swiper styles.

---
+++ assets/scripts/mainBlock.js
@@ -0,0 +1,10 @@
+/* Инициализация Swiper.js для блока mainBlock */
+new Swiper("#mainBlock__wrapper", {
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: "#mainBlock-swiper-pagination",
+  },
+});
+/* /Инициализация Swiper.js для блока mainBlock/ */
